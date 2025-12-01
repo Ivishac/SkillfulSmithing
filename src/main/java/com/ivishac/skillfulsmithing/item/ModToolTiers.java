58ab4ce051a1bfec149aade8eab4f32963654816
Ivishac <ivishacpro@gmail.com>
2025-11-30 18:04:18 -0600

    Added Flint Tools Set and Related Datagen:
    - Introduced Flint tools: Sword, Pickaxe, Axe, Shovel, and Hoe.
    - Added crafting recipes and advancements for tools and their heads.
    - Implemented `Flint Shard` as a new crafting material.
    - Updated datagen to include JSON models, recipes, and loot modifiers.
    
    Signed-off-by: Ivishac <ivishacpro@gmail.com>

--- src/main/java/com/ivishac/skillfulsmithing/item/ModToolTiers.java
+++ src/main/java/com/ivishac/skillfulsmithing/item/ModToolTiers.java
@@ -13,38 +13,41 @@ import java.util.List;
 
 public class ModToolTiers {
 
-    public static final Tier FLINT = TierSortingRegistry.registerTier(
-            new ForgeTier(1, 120, 1f, 1f, 4,
-                    ModTags.Blocks.NEEDS_FLINT_TOOL, () -> Ingredient.EMPTY),
-            new ResourceLocation(SkillfulSmithing.MOD_ID, "flint"), List.of(Tiers.WOOD), List.of(Tiers.STONE));
-
-    public static final Tier COPPER = TierSortingRegistry.registerTier(
-            new ForgeTier(1, 200, 2f, 0.75f, 4,
-                    ModTags.Blocks.NEEDS_COPPER_TOOL, () -> Ingredient.EMPTY),
-            new ResourceLocation(SkillfulSmithing.MOD_ID, "copper"), List.of(Tiers.WOOD), List.of(Tiers.STONE));
-
-    public static final Tier TIN = TierSortingRegistry.registerTier(
-            new ForgeTier(1, 50, 2f, 0.5f, 4,
-                    ModTags.Blocks.NEEDS_TIN_TOOL, () -> Ingredient.EMPTY),
-            new ResourceLocation(SkillfulSmithing.MOD_ID, "tin"), List.of(Tiers.WOOD), List.of(Tiers.STONE));
-
-    public static final Tier BRONZE = TierSortingRegistry.registerTier(
-            new ForgeTier(1, 275, 2f, 1f, 4,
-                    ModTags.Blocks.NEEDS_BRONZE_TOOL, () -> Ingredient.EMPTY),
-            new ResourceLocation(SkillfulSmithing.MOD_ID, "bronze"), List.of(Tiers.WOOD), List.of(Tiers.STONE));
-
-    public static final Tier NICKEL = TierSortingRegistry.registerTier(
-            new ForgeTier(1, 250, 2f, 0.8f, 4,
-                    ModTags.Blocks.NEEDS_NICKEL_TOOL, () -> Ingredient.EMPTY),
-            new ResourceLocation(SkillfulSmithing.MOD_ID, "nickel"), List.of(Tiers.WOOD), List.of(Tiers.STONE));
-
-    public static final Tier IRON = TierSortingRegistry.registerTier(
-            new ForgeTier(1, 325, 2f, 1.3f, 4,
-                    ModTags.Blocks.NEEDS_IRON_TOOL, () -> Ingredient.EMPTY),
-            new ResourceLocation(SkillfulSmithing.MOD_ID, "iron"), List.of(Tiers.WOOD), List.of(Tiers.STONE));
+    // WHEN ADDING A NEW TIER, MAKE SURE TO ADD IT IN THE ORDER OF TIER FROM BEST TO WORST. FOR EXAMPLE, COPPER MUST BE REGISTERED AFTER BRONZE.
+    // IF IT IS BEFORE BRONZE, IT WILL NOT WORK.
 
     public static final Tier STEEL = TierSortingRegistry.registerTier(
             new ForgeTier(1, 575, 2f, 1.5f, 6,
                     ModTags.Blocks.NEEDS_STEEL_TOOL, () -> Ingredient.EMPTY),
-            new ResourceLocation(SkillfulSmithing.MOD_ID, "steel"), List.of(Tiers.WOOD), List.of(Tiers.STONE));
+            new ResourceLocation(SkillfulSmithing.MOD_ID, "steel"), List.of(Tiers.IRON), List.of(Tiers.DIAMOND));
+
+    public static final Tier IRON = TierSortingRegistry.registerTier(
+            new ForgeTier(1, 325, 2f, 1.3f, 4,
+                    ModTags.Blocks.NEEDS_IRON_TOOL, () -> Ingredient.EMPTY),
+            new ResourceLocation(SkillfulSmithing.MOD_ID, "iron"), List.of(Tiers.IRON), List.of(ModToolTiers.STEEL));
+
+    public static final Tier NICKEL = TierSortingRegistry.registerTier(
+            new ForgeTier(2, 221, 3f, 0.8f, 10,
+                    ModTags.Blocks.NEEDS_NICKEL_TOOL, () -> Ingredient.EMPTY),
+            new ResourceLocation(SkillfulSmithing.MOD_ID, "nickel"), List.of(Tiers.GOLD), List.of(Tiers.IRON, ModToolTiers.IRON));
+
+    public static final Tier BRONZE = TierSortingRegistry.registerTier(
+            new ForgeTier(2, 275, 4f, 1f, 5,
+                    ModTags.Blocks.NEEDS_BRONZE_TOOL, () -> Ingredient.EMPTY),
+            new ResourceLocation(SkillfulSmithing.MOD_ID, "bronze"), List.of(Tiers.STONE), List.of(ModToolTiers.NICKEL, Tiers.IRON));
+
+    public static final Tier COPPER = TierSortingRegistry.registerTier(
+            new ForgeTier(1, 180, 3f, 0.75f, 6,
+                    ModTags.Blocks.NEEDS_COPPER_TOOL, () -> Ingredient.EMPTY),
+            new ResourceLocation(SkillfulSmithing.MOD_ID, "copper"), List.of(Tiers.STONE), List.of(ModToolTiers.BRONZE));
+
+    public static final Tier TIN = TierSortingRegistry.registerTier(
+            new ForgeTier(1, 60, 2f, 0.5f, 4,
+                    ModTags.Blocks.NEEDS_TIN_TOOL, () -> Ingredient.EMPTY),
+            new ResourceLocation(SkillfulSmithing.MOD_ID, "tin"), List.of(Tiers.WOOD), List.of(Tiers.STONE));
+
+    public static final Tier FLINT = TierSortingRegistry.registerTier(
+            new ForgeTier(0, 99, 1.9f, 0f, 2,
+                    ModTags.Blocks.NEEDS_FLINT_TOOL, () -> Ingredient.EMPTY),
+            new ResourceLocation(SkillfulSmithing.MOD_ID, "flint"), List.of(Tiers.WOOD), List.of(Tiers.STONE));
 }
